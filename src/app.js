bc8fa81262d51a2ac7de6388519f98bf7885fefb
Zafar Saleem <zafarsaleem3@gmail.com>
2024-09-11 19:12:40 +0500

    style: initial styling of home page, font-awesome

--- src/app.js
+++ src/app.js
@@ -1,6 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import {
+  faPlay,
+  faComment,
+  faDesktop,
+  faLink,
+  faGem,
+  faCode,
+  faShoppingCart,
+} from '@fortawesome/free-solid-svg-icons'
+
+library.add(faPlay, faComment, faDesktop, faLink, faGem, faCode, faShoppingCart)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.use(VueRouter)
 
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,9 +1,14 @@
 <template>
   <div id="container">
-    <div v-if="categories.length">
-      <div v-for="category in categories" :key="category.id">
+    <div v-if="categories.length" class="categories-container">
+      <div v-for="category in categories" :key="category.id" class="category-card">
+        <font-awesome-icon :icon="['fa', category.icon]" class="card-icon" />
         <h2>{{ category.title }}</h2>
-        <span>{{  category.totalArticle }} articles</span>
+        <span class="article-counter">
+          {{ category.totalArticle }}
+          <span v-if="category.totalArticle < 2">article</span>
+          <span v-if="category.totalArticle > 1">articles</span>
+        </span>
         <span>{{ category.updatedOn }}</span>
       </div>
     </div>
@@ -26,6 +31,7 @@ export default {
   methods: {
     async getCategories() {
       const json = await Axios.get("http://localhost:9000/api/categories");
+      console.log(json.data);
       this.categories = json.data;
     },
   },
@@ -36,23 +42,66 @@ export default {
 	@import '../../scss/_variables.scss';
 
 	#container{
-		// text-align: center;
 		font-family: $font-family;
 		color: $text-black;
 		padding: 20px;
+    max-width: 1000px;
+    margin: auto;
 
-		h1{
-			font-style: normal;
-			font-weight: bold;
-			font-size: 34px;
-			line-height: 41px;
-		}
+    .categories-container {
+      display: grid;
+      grid-template-rows: 1fr 1fr;
+      grid-template-columns: 1fr 1fr 1fr;
+      grid-gap: 1em;
 
-		h3{
-			font-style: normal;
-			font-weight: normal;
-			font-size: 20px;
-			line-height: 24px;
-		}
+      .category-card {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 100%;
+        flex-direction: column;
+        width: 315px;
+        height: 220px;
+        top: 359px;
+        left: 148px;
+        gap: 0px;
+        padding: 1em;
+        border: 1px 0px 0px 0px;
+        opacity: 0px;
+        border-width: 0px, 0px, 0px, 0px;
+        border-style: solid;
+        border-color: #EEEEEE;
+        text-align: center;
+
+        .card-icon {
+          width: 43.89px;
+          height: 55px;
+          gap: 0px;
+          opacity: 0px;
+          // background: #4EB54826;
+          // border: 3px solid #03A84E;
+          color: #03A84E;
+        }
+
+        .article-counter {
+          color: #03A84E;
+          margin-bottom: 0.5em;
+        }
+      }
+
+      h1{
+        font-style: normal;
+        font-weight: bold;
+        font-size: 34px;
+        line-height: 41px;
+      }
+
+      h3{
+        font-style: normal;
+        font-weight: normal;
+        font-size: 20px;
+        line-height: 24px;
+      }
+    }
 	}
 </style>
